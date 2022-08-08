3f14951711b9fe3ee9ac45fce47aa267b479b71d
JangHoonSeok <orirori512@naver.com>
2022-08-08 18:34:34 +0900

    initial commit

---
+++ src/routes/Home.js
@@ -0,0 +1,39 @@
+import Movie from "../components/Movie";
+import { useState, useEffect } from "react";
+
+function Home() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
+    setLoading(false); // loading state를 false로 돌려주기!
+    setMovies(json.data.movies);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id} // key는 리액트에서만 map안에서 component들을 render할 때 사용
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+export default Home;
